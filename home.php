5e38f6cc779e438ce1f4fc0170e120ef1847e1b1
Pablo Damico <pablo.damico88@gmail.com>
2014-04-28 01:41:40 -0300

    primer commit - home

---
+++ home.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<meta name="description" content="Saludable con mi pan" />
+	<meta name="keywords" content="etiqueta1, etiqueta2, etiqueta3" />
+	<title>Saludable con mi Pan</title>
+	<link rel="stylesheet" type="text/css" href="estilos.css" title="style" />
+</head>
+<body>
+	<div id="container">
+		<?php include("header.html"); ?>
+	</div>
+	<div class="footer">
+		<img src="images/footer.jpg" />
+	</div>
+</body>
+</html>
