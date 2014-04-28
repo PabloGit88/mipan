9620a3294571cea9518b9b97b1c4ab80302f360d
Diego <songecko@gmail.com>
2014-04-28 19:55:09 -0300

    buscador

--- header.php
+++ header.php
@@ -1,7 +1,21 @@
 <div id="header">
 	<div class="top">
 		<div class="titulo"><img src="images/titulo.png" alt="Saludable con mi Pan" /></div>
-		<div class="buscador"><input name="buscador" type="search"  placeholder=" Busqueda" /></div>				
+		<div class="buscador">
+<script>
+  (function() {
+    var cx = '005301741137347823743:gbpqhdwap-e';
+    var gcse = document.createElement('script');
+    gcse.type = 'text/javascript';
+    gcse.async = true;
+    gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
+        '//www.google.com/cse/cse.js?cx=' + cx;
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(gcse, s);
+  })();
+</script>
+<gcse:search></gcse:search>
+</div>
 	</div>
 	<ul class="menu">
 		<li><a href="#"><img src="images/Mmipan.png"/></a></li>
