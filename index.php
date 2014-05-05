c3ef7434d3acf25402d19059881f80ae1c095954
Diego <songecko@gmail.com>
2014-05-05 14:17:24 -0300

    index

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<meta name="description" content="Saludable con mi pan" />
+	<meta name="keywords" content="saludable, pan" />
+	<title>Saludable con mi Pan</title>
+	<link rel="stylesheet" type="text/css" href="css/estilos.css" title="style" />
+</head>
+<body>
+	<div id="contentHome">
+		<?php include("header.php"); ?>
+	</div>
+	<?php include("footer.php"); ?>
+</body>
+</html>
