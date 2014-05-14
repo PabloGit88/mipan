dd4f5972f6bd55112c45b0522b5c401b63b51236
Diego <songecko@gmail.com>
2014-05-14 02:46:23 -0300

    nuevas modif

--- footer.php
+++ footer.php
@@ -3,9 +3,23 @@
 ?>
 
 <div class="footer">
-	<?php if($uri=="/mipan/mercado.php"){ ?>
-		<img src="images/footer2.jpg" />
+	<?php if(strpos($uri,"mercado.php") === false){ ?>
+		<img class="layer" src="images/footerlayer2.jpg" />
 	<?php }else{ ?>
-		<img src="images/footer.jpg" />
+		<img class="layer" src="images/footerlayer.jpg" />
 	<?php } ?>
+	<div id="footertop">
+	<ul class="listfooter clearfix">
+		<li><a href=""><img src="images/mi_pan_es_de_aqui.jpg"/></a></li>
+		<li><a href="pautas.php"><img src="images/saludable_con_mi_pan.jpg"/></a></a></li>
+		<li><a href="pautas2.php"><img src="images/corazon_de_tomate.jpg"/></a></a></li>
+	</ul>
+	</div>
+	<div id="footermid" class="clearfix">
+		<a href="fraude.php"><img src="images/evita_el_fraude.jpg" /></a>
+		<img src="images/adsef.jpg" />
+	</div>
+	<div id="footerbottom">
+		<img src="images/footerbottom.png" />
+	</div>
 </div>
